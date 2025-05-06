2642917ddf6601c6f5eb80f3fe1b5d0730b741a7
D0wnc <lukas.rausch@edag.com>
2025-05-06 13:25:03 +0200

    String Operations

--- strings/main.py
+++ strings/main.py
@@ -2,5 +2,12 @@ input_String = input("Input a strings: ")
 
 
 if __name__ == "__main__":
-    print(input_String)
     print("Convert to lower ...: " + input_String.lower())
+    print("Capitalize..." + input_String.capitalize())
+    print("Titel conversion.." + input_String.title())
+    print("--------------------------------------------------------------------------")
+    proof = "X" + input_String.strip() + "X"
+    print("strip..." +  input_String.strip() + " Proof:" + proof)
+    print("left.." + input_String.lstrip())
+    print("right.." +  input_String.rstrip())
+    print("--------------------------------------------------------------------------")
