0ccfb0a82f4aa229a0a6645f5af191ea27e25409
D0wnc <dev.lukas.rausch@gmail.com>
2025-05-07 07:02:55 +0200

    numbers complete

---
+++ numbers/main.py
@@ -0,0 +1,11 @@
+import math as m
+
+if __name__ == "__main__":
+    print("numbers with better visuals: coding 1_000_000" + str(1_000_000))
+    print("the normal math operators are obvious so i dont write them out here")
+    print("Modulo Operator from 10/3..." + str(10%3))
+    print("possible numbers with 2^8: ->" + str(2**8))
+    print("sqrt out of 36: " + str(m.sqrt(36)))
+    print("sqrt function result..." + str(type(m.sqrt(36))))
+    print(str(type(1_000)))
+    
